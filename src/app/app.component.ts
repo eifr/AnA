0591f1387168f97a9425fff4422f4ea85745e2a8
eifr <eifr0980@gmail.com>
2019-01-16 22:42:46 +0200

    fix Footer behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,10 @@ export class AppComponent {
       {name: 'description', content: 'Modern Real Estate in Tel Aviv Area'},
       {name: 'author', content: 'eifr'},
       {name: 'title', content: 'תיווך A&A'},
-      {name: 'theme-color', content: '#2c3d4f'}
+      {name: 'theme-color', content: '#2c3d4f'},
+      {property: 'og:title', content: 'A&A RealEstate'},
+      {property: 'og:description', content: 'חברת תיווך מודרנית למציאת נכסים במרכז הארץ'},
+      {property: 'og:image', content: 'https://aa-realestate.co.il/aptImages/assets/ogImage.jpg'}
     ]);
 
     this.title.setTitle('תיווך A&A');
